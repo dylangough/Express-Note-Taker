6fc4d716be22ce656db9ba80dec04c3bfd2b653b
Dylan Gough <dylangough35@gmail.com>
2020-08-22 13:25:04 -0400

    Added server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const path = require('path')
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+require("")(app);
+require("")(app);
+
+app.listen(PORT, () => {
+    console.log("App Listening on PORT: " + PORT);
+})
