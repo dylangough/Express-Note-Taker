ab6bb99f9cf7494cdc5118281eb60a0fd0b8b746
Dylan Gough <dylangough35@gmail.com>
2020-08-22 20:47:47 -0400

    added routes folder

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,16 @@
+var apiRoutes = (app) => {
+
+    app.get("/api/notes", (req, res) => {
+    })
+
+    app.post("/api/notes", (req, res) => {
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+    })
+
+    app.post("/api/clear_notes", (req, res) => {
+    })
+}
+
+module.exports = apiRoutes;
